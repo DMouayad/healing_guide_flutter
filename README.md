78424af88bcfff9bd89b112c71a958807287dc91
Mouayad <muayad.perun@outlook.com>
2024-10-14 15:28:50 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# healing_guide_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/healing_guide_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.healing_guide_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
