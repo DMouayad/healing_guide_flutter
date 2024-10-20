cf6bf5fe45be40736dc4be979f20c15b226312f6
Mouayad <muayad.perun@outlook.com>
2024-10-20 11:33:39 +0300

    feat: add `context` extensions

---
+++ lib/utils/utils.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:healing_guide_flutter/theme/text_theme.dart';
+
+part 'src/context_extensions.dart';
